0ade27c677d121e7d048d6d9f084ba9a470c53d5
Alumno <Alumno@328-15>
2017-04-26 12:31:05 -0500

    Main

---
+++ src/main/Name.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Name {
+    private String name;
+    private String surname;
+
+    public Name(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    
+    
+}
